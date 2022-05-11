403ce838395d607bd3579282f770dd434041e052
Anais Trinca <89652275+trincallegra@users.noreply.github.com>
2022-05-11 23:15:23 +0200

    Nuovo diagramma a torta + rigenerazione di tutte le figure da macOS
    ⚠️ Gli histogrammi risultano rasterizzati in EPS... 🙄
    
    In generale quasi tutte le figure sono molto diverse, anche nei font
    incapsulati nel pdf... santo cielo che disastro! Mi sa che i VTEST se
    ne andranno proprio all'altro mondo.

--- Dati/torta.m
+++ Dati/torta.m
@@ -1,26 +1,29 @@
 %% DRAFT... 
 
-% Load Data
-  load WRANGLED.mat
+ % Load Data
+   load WRANGLED.mat
   
-  labels = {'EV','IN','OS+IN','IM'};
-  slices = categorical(DATA.VIADISOMMINISTRAZIONE,[1 2 3 4],labels);
+   labels = {'EV','IN','OS+IN','IM'};
+   slices = categorical(DATA.VIADISOMMINISTRAZIONE,[1 2 3 4],labels);
   
-  p = pie(slices,[],labels);
+   p = pie(slices);
   
-  for i = 1:length(labels)
-      text = p(i*2);
-      text.FontSize = 20;
-  end
-  %{
-  hex = ['#502064'  ;
-         '#8267BE'  ; 
-         '#3FA796'  ;
-         '#FFBD35'] ; 
-  %}
-  hex = ['#502064'  ;
-         '#8267BE'  ; 
-         '#3FA796'  ;
-         '#FFBD35'] ; 
-  colormap(hex2rgb(hex));
+   for i = 1:length(labels)
+       text = p(i*2);
+       text.FontSize = 20;
+   end
+ %{
+   hex = ['#502064'  ;
+          '#8267BE'  ; 
+          '#3FA796'  ;
+          '#FFBD35'] ; 
+ %}
+   hex = ['#502064'  ;
+          '#8267BE'  ; 
+          '#3FA796'  ;
+          '#FFBD35'] ; 
+   colormap(hex2rgb(hex));
+  
+ % Save figures
+   export_fig(gcf,'../Figure/somministrazione')
   
